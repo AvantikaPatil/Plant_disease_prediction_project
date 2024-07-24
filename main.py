e8d1406450fb41c1f782961b10e1a6d2246ef3b6
Avantika Patil <110914988+AvantikaPatil@users.noreply.github.com>
2024-07-24 11:57:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 # Tensorflow model prediction
 
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_model/trained_model.h5")
+    model = tf.keras.models.load_model("trained_model\trained_model.h5")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])   # convert single image to array
